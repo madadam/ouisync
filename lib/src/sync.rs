d3d15b36d071047ffa02f19ad023ef7ce129b9ca
Adam Cigánek <adam.ciganek@proton.me>
2023-12-13 09:01:37 +0100

    Implement DelayMap pause/resume

--- lib/src/sync.rs
+++ lib/src/sync.rs
@@ -590,14 +590,16 @@ pub(crate) mod delay_map {
     use std::{
         borrow::Borrow,
         hash::Hash,
-        task::{ready, Context, Poll},
+        mem,
+        task::{ready, Context, Poll, Waker},
     };
-    use tokio::time::Duration;
+    use tokio::time::{Duration, Instant};
     use tokio_util::time::delay_queue::{DelayQueue, Key};
 
     pub struct DelayMap<K, V> {
         items: HashMap<K, Item<V>>,
         delays: DelayQueue<K>,
+        state: State,
     }
 
     impl<K, V> DelayMap<K, V> {
@@ -605,6 +607,7 @@ pub(crate) mod delay_map {
             Self {
                 items: HashMap::default(),
                 delays: DelayQueue::default(),
+                state: State::Active,
             }
         }
     }
@@ -661,9 +664,59 @@ pub(crate) mod delay_map {
             self.items.is_empty()
         }
 
+        /// Pauses the timeouts. No item expires while the map is paused.
+        pub fn pause(&mut self) {
+            match self.state {
+                State::Active => {
+                    self.state = State::Paused {
+                        since: Instant::now(),
+                        waker: None,
+                    }
+                }
+                State::Paused { .. } => (),
+            }
+        }
+
+        /// Resumes the timeout if paused (does nothing if not paused). The time the map's been
+        /// paused doesn't count towards the timeouts. For example: an item is scheduled for 1s,
+        /// then 0.5s passes and then the map is paused. Another 0.5s passes but the item doesn't
+        /// expire yet. Then the map is resumed. The item still doesn't expire because it still had
+        /// 0.5s left in its timeout then the map was paused. So the item expires only after
+        /// another 0.5s.
+        pub fn resume(&mut self) {
+            let state = mem::replace(&mut self.state, State::Active);
+
+            let (since, waker) = match state {
+                State::Active => return,
+                State::Paused { since, waker } => (since, waker),
+            };
+
+            for item in self.items.values_mut() {
+                let expired = self.delays.remove(&item.delay_key);
+                let deadline = expired.deadline();
+                let key = expired.into_inner();
+
+                let timeout = deadline.saturating_duration_since(since);
+                item.delay_key = self.delays.insert(key, timeout);
+            }
+
+            // Wake
+            if let Some(waker) = waker {
+                waker.wake();
+            }
+        }
+
         /// Poll for the next expired item. This can be wrapped in `future::poll_fn` and awaited.
         /// Returns `Poll::Ready(None)` if the map is empty.
         pub fn poll_expired(&mut self, cx: &mut Context<'_>) -> Poll<Option<(K, V)>> {
+            match &mut self.state {
+                State::Active => (),
+                State::Paused { waker, .. } => {
+                    waker.get_or_insert_with(|| cx.waker().clone());
+                    return Poll::Pending;
+                }
+            }
+
             if let Some(expired) = ready!(self.delays.poll_expired(cx)) {
                 let key = expired.into_inner();
                 // unwrap is OK because an entry exists in `delays` iff it exists in `items`.
@@ -673,6 +726,11 @@ pub(crate) mod delay_map {
             } else {
                 Poll::Ready(None)
             }
+        }
+
+        #[cfg(test)]
+        pub async fn expired(&mut self) -> Option<(K, V)> {
+            std::future::poll_fn(|cx| self.poll_expired(cx)).await
         }
     }
 
@@ -700,5 +758,71 @@ pub(crate) mod delay_map {
     struct Item<V> {
         value: V,
         delay_key: Key,
+    }
+
+    enum State {
+        Active,
+        Paused {
+            since: Instant,
+            waker: Option<Waker>,
+        },
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use futures_util::FutureExt;
+        use tokio::time;
+
+        #[tokio::test(start_paused = true)]
+        async fn smoke() {
+            let mut map = DelayMap::new();
+
+            map.insert("foo", 1, Duration::from_millis(200));
+            map.insert("bar", 2, Duration::from_millis(100));
+
+            let start = Instant::now();
+            assert_eq!(map.expired().await, Some(("bar", 2)));
+            assert_eq!(start.elapsed(), Duration::from_millis(100));
+
+            assert_eq!(map.expired().await, Some(("foo", 1)));
+            assert_eq!(start.elapsed(), Duration::from_millis(200));
+
+            assert_eq!(map.expired().await, None);
+        }
+
+        #[tokio::test(start_paused = true)]
+        async fn pause_resume() {
+            let mut map = DelayMap::new();
+
+            map.insert("foo", 1, Duration::from_millis(100));
+            map.pause();
+
+            time::sleep(Duration::from_millis(200)).await;
+
+            // The item would've been returned here had the map not been paused.
+            assert_eq!(map.expired().now_or_never(), None);
+
+            map.resume();
+
+            // The time the map was paused doesn't count towards the timeout.
+            assert_eq!(map.expired().now_or_never(), None);
+
+            time::sleep(Duration::from_millis(100)).await;
+            assert_eq!(map.expired().now_or_never(), Some(Some(("foo", 1))));
+
+            map.insert("bar", 2, Duration::from_millis(100));
+
+            // Let the item expire without polling the map.
+            time::sleep(Duration::from_millis(200)).await;
+
+            map.pause();
+            assert_eq!(map.expired().now_or_never(), None);
+
+            // The item had already been expired when the map was paused so it's returned
+            // immediatelly after resume.
+            map.resume();
+            assert_eq!(map.expired().now_or_never(), Some(Some(("bar", 2))));
+        }
     }
 }
